a3a9038776495b88b8ab665b5900315ad6a445e9
Luskalves <lusca.alves32@gmail.com>
2022-05-25 13:35:47 -0300

    nome do pokemon agora aparece na tela

--- src/pages/SeachPage.js
+++ src/pages/SeachPage.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import findPokemonApi from './api/findPokemonApi';
+import '../App.css'
 
 function SearchPage () {
   const [pokeImage, setPokeImage] = useState()
+  const [pokeName, setPokeName] = useState('')
 
   const findByName = async ({ target }) => {
     const response = await findPokemonApi(target.value)
     if (response) {
+      setPokeName(response.species.name)
       setPokeImage(response.sprites.front_default);
     }
   };
@@ -15,7 +18,9 @@ function SearchPage () {
     const iniciais = ["bulbasaur", "squirtle", "charmander"]
     const escolhido = Math.floor(Math.random() * 3);
     const resultado = await findPokemonApi(iniciais[escolhido])
+    
     setPokeImage(resultado.sprites.front_default);
+    setPokeName(resultado.species.name)
   }
 
   useEffect(() => {
@@ -23,7 +28,7 @@ function SearchPage () {
   }, [])
 
   return (
-    <div>
+    <div className="searchbar">
       <h1> POKE LUSKA </h1>
       <input
         type="text"
@@ -31,6 +36,7 @@ function SearchPage () {
         placeholder="procure um pokemon"
       />
       <img src={ pokeImage } alt="foto do pokemon" />
+      <span> {pokeName} </span>
     </div>
   );
 }
