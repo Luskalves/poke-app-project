4bf5ac68fadddfc9ccba93ba6857f6edc4e8eaff
Luskalves <lusca.alves32@gmail.com>
2022-05-25 11:05:42 -0300

    pesquisa de pokemons funcionando

--- src/pages/SeachPage.js
+++ src/pages/SeachPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import findPokemonApi from './api/findPokemonApi';
 
 function SearchPage () {
+  const [selectedPokemon, setSelectedPokemon] = useState()
+
+  const findByName = async ({ target }) => {
+    const teste = await findPokemonApi(target.value)
+    setSelectedPokemon(teste);
+  }
+    // useEffect((
+    //   console.log('foi')
+    // ), [])
+
   return (
     <div>
+      {console.log(selectedPokemon)}
       <h1> POKE LUSKA </h1>
-      <input type="text" placeholder="procure um pokemon" />
+      <input
+        type="text"
+        onChange={ findByName }
+        placeholder="procure um pokemon"
+      />
     </div>
   );
 }
--- src/pages/api/findPokemonApi.js
+++ src/pages/api/findPokemonApi.js
@@ -1,5 +1,9 @@
 export default async function findPokemonApi(nameOrId) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrId}`)
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrId}`)
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return [];
+  }
 }
